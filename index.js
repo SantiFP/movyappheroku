57c0ccc4f2b92824a9575206515e34f4e8497513
unknown <santifp1997@gmail.com>
2021-10-29 01:14:54 -0300

    ya fue

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req,res)=>{
     res.send('este es el test')
 })
 
+app.get('/ruta', (req,res)=>{
+    res.send('otra ruta')
+})
+
 app.listen(PORT, () =>{
     console.log("Server working")
 })
