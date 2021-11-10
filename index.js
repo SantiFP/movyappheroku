d2627cde496cf1b1e210017b834fc4d891feb981
unknown <santifp1997@gmail.com>
2021-11-10 18:49:51 -0300

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movyapp');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 
 const path = require('path')
